a6ff77a374b3a9c086c903f3e60495cf2e2735ef
atgotreaux <atgotreaux@users.noreply.github.com>
2024-09-17 23:40:26 -0500

    fix: use spring security for secure header configuration

---
+++ src/main/java/com/gotreaux/site/configuration/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.gotreaux.site.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.headers(headers -> headers.httpStrictTransportSecurity(hsts -> hsts.preload(true).maxAgeInSeconds(63072000L)))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return _ -> {
+            throw new UnsupportedOperationException("Authentication is disabled");
+        };
+    }
+}
--- src/main/java/com/gotreaux/site/filters/package-info.java
+++
@@ -1,6 +0,0 @@
-@NonNullApi
-@NonNullFields
-package com.gotreaux.site.filters;
-
-import org.springframework.lang.NonNullApi;
-import org.springframework.lang.NonNullFields;
